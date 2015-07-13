4178f8330a2382a4d5c1e5d1ac7e37b98023f3ed
t321 <sallonara@gmail.com>
2015-07-13 18:35:09 +0000

    - Añadida la página autores, en la que se muestra el nombre del creador y la foto.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var author = require('../controllers/author');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,5 +11,6 @@ router.get('/', function(req, res, next) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', author.author);
 
 module.exports = router;
