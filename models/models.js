eec885607b0af6dde729e0957e2fc927397ca6ea
t321 <sallonara@gmail.com>
2015-07-30 01:32:24 +0000

    - Añadido múltiples preguntas y respuestas.

--- models/models.js
+++ models/models.js
@@ -38,12 +38,16 @@ sequelize.sync().then(function () {
     // success(...) ejecuta el manejador una vez creada la tabla
     Quiz.count().then(function (count) {
         if(count === 0){
-            return Quiz.create({
+            Quiz.create({
                 pregunta: 'Capital de Italia',
                 respuesta: 'Roma'
+            });
+            Quiz.create({
+                pregunta: 'Capital de Portugal',
+                respuesta: 'Lisboa'
             }).then(function () {
                 console.log('Base de datos inicializada');
             });
-        }
+        };
     });
 });
